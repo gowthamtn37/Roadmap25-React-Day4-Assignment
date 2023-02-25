f6b4b6de5a90e73da5e9618df7ed10bfa2d38217
gowthamtn37 <gowthamtn37@gmail.com>
2023-02-25 17:21:18 +0530

    new

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -9,22 +9,17 @@ export function Dashboard() {
     <div>
       <div id="wrapper">
         <Sidebar />
-
-        {/* <!-- Content Wrapper --> */}
         <div id="content-wrapper" className="d-flex flex-column">
           <Topbar />
           <MainContent />
           <Footer />
         </div>
-        {/* <!-- End of Content Wrapper --> */}
       </div>
-      {/* <!-- End of Page Wrapper --> */}
 
       {/* <!-- Scroll to Top Button--> */}
       <a className="scroll-to-top rounded" href="Topbar">
         <i className="fas fa-angle-up"></i>
       </a>
-
       <LogoutModal />
     </div>
   );
